af2d9e95f47df52dbb1a7a87d165bf07af10e621
AinaRafaly21 <Ramavo.nyaina@gmail.com>
2022-04-04 20:31:21 +0200

    Update test.js

--- test.js
+++ test.js
@@ -6,7 +6,7 @@ app.use(express.static("public"))
 
 // define the first route
 app.get("/", function (req, res) {
-  res.send("<h1>Hello World!</h1>")
+  res.send("<h1>coucou njuuu</h1>")
 })
 
 // start the server listening for requests
